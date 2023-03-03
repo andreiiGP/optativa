224fe26a4524e3f58c9c8f606f49509967be093d
andreiiGP <andru0829@gmail.com>
2023-03-02 20:19:20 -0500

    formulario creacion

--- formulario.php
+++ formulario.php
@@ -1,18 +1,37 @@
+<?php
+           
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>formuario php </title>
 </head>
 <body>
 
-        <?php
-            echo " hola mundo en PHP ";
-        ?>
+        <h1>formulario de contacto php</h1>
 
- 
+        <form method="post" action="">
+        
+        Correo electronico:
+        <br>
+        <input type="text" name="email" value="">
+        <?php echo $email_error; ?>
+        <br>
+        <br>
+        mensaje
+        <br>
+        <textarea name="mensaje"> <?php echo $mensaje_error ; ?> </textarea>
+        <?php echo $email_error; ?>
+        <br>
+        <br>
+        <input type="submit" value="submit">
+        
+        </from>
     
 </body>
 </html>
